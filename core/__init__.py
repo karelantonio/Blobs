456c070101c30d8da268e63628458e9ee1907c27
Karel <karelantoniogz@gmail.com>
2025-12-29 16:43:19 -0500

    fix: Minor fixes

--- core/__init__.py
+++ core/__init__.py
@@ -2,9 +2,11 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import ObjectProperty
 
 from .common import *
+from .state import *
 
 
 class MaterialButton(Button):
@@ -15,6 +17,13 @@ class StartScreen(Screen):
     pass
 
 
+class RootWidget(ScreenManager):
+    state = ObjectProperty(State())
+
+
 class MainApp(App):
+    rootw = None
+
     def build(self):
-        return self.root
+        self.rootw = RootWidget()
+        return self.rootw
--- core/state.py
+++ core/state.py
@@ -1,13 +1,17 @@
 from dataclasses import dataclass
 from kivy.app import App
+from kivy.event import EventDispatcher
 
-@dataclass
-class State:
-    pass
+class State(EventDispatcher):
+    def __init__(self, *args, **kwa):
+        super().__init__(*args, *kwa)
+        # Initialize here
+
 
 def state()->State:
-    return App.get_running_app().state
+    return App.get_running_app().rootw.state
+
 
 def set_state(st: State):
-    App.get_running_app().state = st
+    App.get_running_app().rootw.state = st
 
