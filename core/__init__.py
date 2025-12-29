ea00db4138eb94eb890f938e4da635c852a02d47
Karel <karelantoniogz@gmail.com>
2025-12-29 16:52:37 -0500

    chore: Refactor and minor changes

--- core/__init__.py
+++ core/__init__.py
@@ -1,16 +1,11 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import ObjectProperty
 
 from .common import *
 from .state import *
-
-
-class MaterialButton(Button):
-    pass
 
 
 class StartScreen(Screen):
--- core/common.py
+++ core/common.py
@@ -1,4 +1,11 @@
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
 class Blob(Widget):
     pass
+
+
+class MaterialButton(Button):
+    pass
+
+
