92afe1c340a56e6264d07e8107dcea542b5eabdb
Karel <karelantoniogz@gmail.com>
2025-12-29 14:21:53 -0500

    feat: Added material button component

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,13 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 
 from .common import *
+
+
+class MaterialButton(Button):
+    pass
 
 
 class StartScreen(AnchorLayout):
