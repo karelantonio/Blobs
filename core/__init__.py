8326cec7d974a6c477d96b3dd7dfd468a6791b06
Karel <karelantoniogz@gmail.com>
2026-02-01 00:57:05 -0500

    feat: Able to specify amount of each resource

--- core/__init__.py
+++ core/__init__.py
@@ -20,6 +20,18 @@ from .resources import RESOURCES, RESOURCES_AS_DICT
 from .events import EVENTS
 
 
+class EventResQuantityPicker(MDDialog):
+    name = StringProperty("")
+    qty = NumericProperty(0)
+    evt_selected = ObjectProperty(lambda x: None)
+
+    def get_total(self):
+        return RESOURCES_AS_DICT[self.name].total
+
+    def on_ok(self):
+        self.dismiss()
+        self.evt_selected(int(self.ids.slider.value))
+
 class EventChooserDialogItem(MDListItem):
     image = StringProperty("")
     title = StringProperty("")
@@ -56,17 +68,30 @@ class NewEventRes(RectangularRippleBehavior, ButtonBehavior, MDBoxLayout):
     name = StringProperty("")
     icon = StringProperty("")
     qty  = NumericProperty(0)
+    qty_text = StringProperty("0 x")
+    evt_qty_update = ObjectProperty(lambda x, y: None)
+
+    def show_qnt_picker(self):
+        def on_selected(new_qty):
+            self.evt_qty_update(self.name, new_qty)
+        EventResQuantityPicker(name=self.name, qty=self.qty, evt_selected=on_selected).open()
 
 class NewEventScreen(MDScreen):
-    resources = ListProperty([{
-        "name": res.name,
-        "icon": RESOURCES_AS_DICT[res.name].icon,
-        "qty": 0,
-    } for res in RESOURCES])
+    resources = ListProperty()
     event_tpl = None
     event_tpl_name = StringProperty("Pick an event")
     event_tpl_description = StringProperty("")
     event_tpl_icon = StringProperty("")
+
+    def __init__(self, *args, **kwa):
+        super().__init__(*args, **kwa)
+        self.resources = [{
+            "name": res.name,
+            "icon": RESOURCES_AS_DICT[res.name].icon,
+            "evt_qty_update": self.evt_qty_update,
+            "qty": 0,
+            "qty_text": "0 x",
+        } for res in RESOURCES]
 
     def set_event_tpl(self, ev):
         self.event_tpl = ev
@@ -116,6 +141,17 @@ class NewEventScreen(MDScreen):
 
         timp = MDTimePickerDialVertical(on_ok=on_ok, on_cancel=on_cancel)
         timp.open()
+
+    def evt_qty_update(self, name, new_qty):
+        copy = self.resources.copy()
+        for i, e in enumerate(self.resources):
+            if e["name"] == name:
+                copy[i]["qty"] = new_qty
+                copy[i]["qty_text"] = f"{new_qty} x"
+                break
+        self.resources = copy
+        self.ids.resources_rv.data = copy
+        self.ids.resources_rv.refresh_from_data()
 
 class RootWidget(MDScreenManager):
     state = ObjectProperty(State())
